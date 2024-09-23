5f9bfeffe0ab0ec4366ec8a1dc12e685a8c31a11
Josechalo2024 <jrevolorioc6@miumg.edu.gt>
2024-09-22 22:05:10 -0600

    Delete src directory

--- src/empleados/dto/actualizar-empleado.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CrearEmpleadoDto } from './crear-empleado.dto';
-
-export class ActualizarEmpleadoDto extends PartialType(CrearEmpleadoDto) {}
